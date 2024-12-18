4a9ed0fdd049857efd3b15a016e23480ae924165
Harshit Sah <harshitsah96@gmail.com>
2024-12-18 16:02:27 +0530

    Update to serving files

--- app.js
+++ app.js
@@ -25,13 +25,27 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("combined",{stream:accessLogStream}))
-
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use("/expense",authentication);
 app.use("/expense",expenses);
 app.use("/purchase",premium);
 app.use("/premium",premium);
 app.use("/password",authentication);
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname, 'views','index.html'));
+
+});
+app.get('*',(req,res)=>{
+  const requestedUrl = req.url;
+  if(requestedUrl.startsWith('/views/')){
+  res.sendFile(path.join(__dirname,requestedUrl));
+  
+  }else{
+    res.sendFile(path.join(__dirname,'public',requestedUrl));
+  }
+});
+
 User.hasMany(Expenses);
 Expenses.belongsTo(User);
 User.hasMany(Order);
