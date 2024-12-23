15e97df46e2bedeb9dc646de8a5f543017df19dc
Harshit Sah <harshitsah96@gmail.com>
2024-12-23 15:47:22 +0530

    external file error solving

--- public/js/index.js
+++ public/js/index.js
@@ -29,34 +29,3 @@ document.getElementById('signupForm').addEventListener('submit',async(event)=>{
 });
 
 
-
-document.getElementById('loginForm').addEventListener('submit',async(event)=>{
-  alert('hello');
-  event.preventDefault();
-  const password = document.querySelector("#loginpassword").value;
-  const email = document.querySelector("#loginemail").value;
-  const errormsg = document.querySelector("#error");
-  try {
-    const response = await axios.post("https://hsexpensetracker.duckdns.org/expense/login", {
-      email: email,
-      password: password,
-    });
-    if (response.status == 200) {
-   
-      localStorage.setItem('AuthToken',response.data.token);
-      window.location.assign('expense.html');
-    }
-  } catch (error) {
- 
-    if (error.status == 401 || error.status == 404) {
-      //console.log("Error during login:", error.response.data.message);
-      errormsg.innerHTML = `<p style="font-size:20px ;color:red">Login failed, ${error.response.data.message}</p> `;
-    } else {
-      
-      errormsg.innerHTML = `<p style="font-size:16px ;color:red">Login failed, ${error.response.data.message}</p> `;
-    }
-  }
-});
-
-
-
