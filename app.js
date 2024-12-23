a3f28ae672a1d602c0f55eccdb85b6ba75cafbc8
Harshit Sah <harshitsah96@gmail.com>
2024-12-23 15:38:46 +0530

    external file error solving

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const path = require('path');
-const fs = require('fs');
-require('dotenv').config();
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config();
 
 const db = require("./utils/database");
 
-const authentication = require('./routes/authenticationRoute');
-const expenses = require('./routes/expenseRoute');
-const premium = require('./routes/premiumRoute');
+const authentication = require("./routes/authenticationRoute");
+const expenses = require("./routes/expenseRoute");
+const premium = require("./routes/premiumRoute");
 
 const User = require("./models/Users");
 const Expenses = require("./models/Expenses");
@@ -18,46 +18,48 @@ const Order = require("./models/Orders");
 const ForgotPasswordRequest = require("./models/ForgotPasswordRequest");
 const FileUrls = require("./models/FileUrls");
 
-
-const app= express();
-const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});
+const app = express();
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet({
-  contentSecurityPolicy:{
-  directives:{
-    directives: {
-      defaultSrc: ["'self'"], // Allows resources from the same origin
-      styleSrc: ["'self'", 'https://cdn.jsdelivr.net'], // Allows styles from self and the CDN
-      scriptSrc: ["'self'", "'unsafe-inline'"], // Example for scripts
-      imgSrc: ["'self'", 'data:'], // Allow images from self and inline data URIs
-  }
-  
-  }
-  }
-}));
-app.use(morgan("combined",{stream:accessLogStream}))
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, 'views')));
-app.use("/expense",authentication);
-app.use("/expense",expenses);
-app.use("/purchase",premium);
-app.use("/premium",premium);
-app.use("/password",authentication);
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], // Allows resources from the same origin
+        styleSrc: ["'self'", "https://cdn.jsdelivr.net"], // Allows styles from self and the CDN
+        scriptSrc: ["'self'", "'unsafe-inline'"], // Example for scripts
+        imgSrc: ["'self'", "data:"], // Allow images from self and inline data URIs
+      },
+    },
+  })
+);
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "views")));
+app.use("/expense", authentication);
+app.use("/expense", expenses);
+app.use("/purchase", premium);
+app.use("/premium", premium);
+app.use("/password", authentication);
 
-app.get('/',(req,res)=>{
-res.sendFile(path.join(__dirname, 'views','index.html'));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-app.get('*',(req,res)=>{
+app.get("*", (req, res) => {
   const requestedUrl = req.url;
-  const filePath = requestedUrl.startsWith('views')?path.join(__dirname,requestedUrl):path.join(__dirname,'public',requestedUrl);
-  res.sendFile(filePath,(err)=>{
+  const filePath = requestedUrl.startsWith("views")
+    ? path.join(__dirname, requestedUrl)
+    : path.join(__dirname, "public", requestedUrl);
+  res.sendFile(filePath, (err) => {
     if (err) {
-      console.error('Error serving file:', err.message);
-      res.status(404).send('File not found');
-  }
+      console.error("Error serving file:", err.message);
+      res.status(404).send("File not found");
+    }
   });
 });
 
@@ -65,22 +67,18 @@ User.hasMany(Expenses);
 Expenses.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(User);
-User.hasMany(ForgotPasswordRequest,{
-foreignKey:'userId',
-onDelete:'CASCADE',
+User.hasMany(ForgotPasswordRequest, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
 });
-ForgotPasswordRequest.belongsTo(User,{
-  foreignKey:'userId',
+ForgotPasswordRequest.belongsTo(User, {
+  foreignKey: "userId",
 });
 User.hasMany(FileUrls);
 FileUrls.belongsTo(User);
-
 
 db.sync()
   .then(() => {
     app.listen(process.env.PORT);
   })
   .catch((err) => console.log(err));
-
-
-
